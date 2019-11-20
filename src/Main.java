d7850916a591b6a00944789c613d0e68172c942e
mf1849x <37416652+mf1849x@users.noreply.github.com>
2019-11-20 09:17:36 -0800

    Update Main.java
    Add time

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   //Start here
-  //November 20, 2019
+  //November 20, 2019 - 9:00 AM
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
